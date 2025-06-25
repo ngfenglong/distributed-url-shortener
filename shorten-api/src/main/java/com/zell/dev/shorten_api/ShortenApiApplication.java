21d6d1f3c111d08bc58eb73bc26cefe4040e494f
Zell <fenglong1502@hotmail.com>
2025-06-25 08:05:43 +0800

    shorten api project initialisastion

---
+++ shorten-api/src/main/java/com/zell/dev/shorten_api/ShortenApiApplication.java
@@ -0,0 +1,13 @@
+package com.zell.dev.shorten_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenApiApplication.class, args);
+	}
+
+}
