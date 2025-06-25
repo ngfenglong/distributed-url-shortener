13c812be70f2d82d89e53b2f8c355ba3f76d6c59
Zell <fenglong1502@hotmail.com>
2025-06-25 08:05:33 +0800

    redirect-api project initialisation

---
+++ redirect-api/src/main/java/com/zell/dev/redirect_api/RedirectApiApplication.java
@@ -0,0 +1,13 @@
+package com.zell.dev.redirect_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectApiApplication.class, args);
+	}
+
+}
---
+++ redirect-api/src/test/java/com/zell/dev/redirect_api/RedirectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zell.dev.redirect_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
