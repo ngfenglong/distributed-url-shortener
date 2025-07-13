04779a59167d6c2cf06a367928ff8f16109045f0
Zell <fenglong1502@hotmail.com>
2025-07-14 07:33:56 +0800

    Initialise Eureka Server

---
+++ eureka-server/src/test/java/com/zell/dev/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zell.dev.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
